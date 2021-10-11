030b42a986e1affeea85620196c2d195cfc62cb0
renu1233333 <renu.xworkz@gmail.com>
2021-10-11 16:21:45 +0530

    files uploded

---
+++ mutatorOrSetter_method/Tubelight.java
@@ -0,0 +1,25 @@
+class Tubelight
+{
+private String name;
+private int watts;
+String getName()
+{
+return this.name;
+}
+
+int getWatts()
+{
+return this.watts;
+}
+void setName(String name)
+{
+	this.name=name;
+}
+
+void setWatts(int watts)
+{
+	this.watts=watts;
+	
+}
+
+}
