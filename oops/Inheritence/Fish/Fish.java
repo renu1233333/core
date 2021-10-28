cdd45ba09b1e701301c536d4dede7f91f15a6437
renu1233333 <renu.xworkz@gmail.com>
2021-10-28 11:18:59 +0530

    files uploded

---
+++ oops/Inheritence/Fish/Fish.java
@@ -0,0 +1,11 @@
+package com.xworkz.fish;
+
+public class Fish {
+public String region="southeast";
+public boolean alive=true;
+
+public Fish() {
+	System.out.println("Invoking Fish no argument constructor");
+}
+
+}
